b85fc14a227079342c22c1313bd925453ca31655
Alexander Jones <happy5214@gmail.com>
2015-06-11 16:56:19 -0500

    [WIP] Load Flow object content in Pywikibot
    This change allows Pywikibot bots to load basic Flow content from
    Flow-enabled wikis. This includes loading the contents of posts,
    loading topics from boards, and loading replies to topics and posts.
    Tests have been added to ensure that content is indeed loaded.
    
    Bug: T101260
    Bug: T101261
    Bug: T101262
    Bug: T101263
    Change-Id: Ibd84d0763a7745da6410c194b8d5ddf9ba8b57de

--- tests/flow_tests.py
+++ tests/flow_tests.py
@@ -11,6 +11,7 @@ __version__ = '$Id$'
 
 import pywikibot
 import pywikibot.flow
+import pywikibot.tools
 
 from tests.aspects import (
     TestCase,
@@ -21,10 +22,13 @@ from tests.basepage_tests import (
     BasePageLoadRevisionsCachingTestBase,
 )
 
+if not pywikibot.tools.PY2:
+    unicode = str
+
 
 class TestBoardBasePageMethods(BasePageMethodsTestBase):
 
-    """Test Flow pages using BasePage-defined methods."""
+    """Test Flow board pages using BasePage-defined methods."""
 
     family = 'mediawiki'
     code = 'mediawiki'
@@ -35,14 +39,38 @@ class TestBoardBasePageMethods(BasePageMethodsTestBase):
         super(TestBoardBasePageMethods, self).setUp()
 
     def test_basepage_methods(self):
-        """Test basic Page methods on a Flow page."""
+        """Test basic Page methods on a Flow board page."""
         self._test_invoke()
         self._test_return_datatypes()
-        self.assertEqual(self._page.isRedirectPage(), False)
+        self.assertFalse(self._page.isRedirectPage())
         self.assertEqual(self._page.latest_revision.parent_id, 0)
 
     def test_content_model(self):
         """Test Flow page content model."""
+        self.assertEqual(self._page.content_model, 'flow-board')
+
+
+class TestTopicBasePageMethods(BasePageMethodsTestBase):
+
+    """Test Flow topic pages using BasePage-defined methods."""
+
+    family = 'mediawiki'
+    code = 'mediawiki'
+
+    def setUp(self):
+        self._page = pywikibot.flow.Topic(
+            self.site, 'Topic:Sh6wgo5tu3qui1w2')
+        super(TestTopicBasePageMethods, self).setUp()
+
+    def test_basepage_methods(self):
+        """Test basic Page methods on a Flow topic page."""
+        self._test_invoke()
+        self._test_return_datatypes()
+        self.assertFalse(self._page.isRedirectPage())
+        self.assertEqual(self._page.latest_revision.parent_id, 0)
+
+    def test_content_model(self):
+        """Test Flow topic page content model."""
         self.assertEqual(self._page.content_model, 'flow-board')
 
 
@@ -75,14 +103,14 @@ class TestFlowLoading(TestCase):
     def test_board_uuid(self):
         """Test retrieval of Flow board UUID."""
         site = self.get_site()
-        board = pywikibot.flow.Board(site, u'Talk:Sandbox')
-        self.assertEqual(board.uuid, u'rl7iby6wgksbpfno')
+        board = pywikibot.flow.Board(site, 'Talk:Sandbox')
+        self.assertEqual(board.uuid, 'rl7iby6wgksbpfno')
 
     def test_topic_uuid(self):
         """Test retrieval of Flow topic UUID."""
         site = self.get_site()
-        topic = pywikibot.flow.Topic(site, u'Topic:Sh6wgo5tu3qui1w2')
-        self.assertEqual(topic.uuid, u'sh6wgo5tu3qui1w2')
+        topic = pywikibot.flow.Topic(site, 'Topic:Sh6wgo5tu3qui1w2')
+        self.assertEqual(topic.uuid, 'sh6wgo5tu3qui1w2')
 
     def test_post_uuid(self):
         """Test retrieval of Flow post UUID.
@@ -92,6 +120,48 @@ class TestFlowLoading(TestCase):
         stored properly.
         """
         site = self.get_site()
-        topic = pywikibot.flow.Topic(site, u'Topic:Sh6wgo5tu3qui1w2')
-        post = pywikibot.flow.Post(topic, u'sh6wgoagna97q0ia')
-        self.assertEqual(post.uuid, u'sh6wgoagna97q0ia')
+        topic = pywikibot.flow.Topic(site, 'Topic:Sh6wgo5tu3qui1w2')
+        post = pywikibot.flow.Post(topic, 'sh6wgoagna97q0ia')
+        self.assertEqual(post.uuid, 'sh6wgoagna97q0ia')
+
+    def test_post_contents(self):
+        """Test retrieval of Flow post contents."""
+        # Load
+        site = self.get_site()
+        topic = pywikibot.flow.Topic(site, 'Topic:Sh6wgo5tu3qui1w2')
+        post = pywikibot.flow.Post(topic, 'sh6wgoagna97q0ia')
+        # Wikitext
+        wikitext = post.get(format='wikitext')
+        self.assertIn('wikitext', post._content)
+        self.assertNotIn('html', post._content)
+        self.assertIsInstance(wikitext, unicode)
+        self.assertNotEqual(wikitext, '')
+        # HTML
+        html = post.get(format='html')
+        self.assertIn('html', post._content)
+        self.assertIn('wikitext', post._content)
+        self.assertIsInstance(html, unicode)
+        self.assertNotEqual(html, '')
+        # Caching (hit)
+        post._content['html'] = 'something'
+        html = post.get(format='html')
+        self.assertIsInstance(html, unicode)
+        self.assertEqual(html, 'something')
+        self.assertIn('html', post._content)
+        # Caching (reload)
+        post._content['html'] = 'something'
+        html = post.get(format='html', force=True)
+        self.assertIsInstance(html, unicode)
+        self.assertNotEqual(html, 'something')
+        self.assertIn('html', post._content)
+
+    def test_topiclist(self):
+        """Test loading of topiclist."""
+        site = self.get_site()
+        board = pywikibot.flow.Board(site, 'Talk:Sandbox')
+        i = 0
+        for topic in board.topics(limit=7):
+            i += 1
+            if i == 10:
+                break
+        self.assertEqual(i, 10)
